9b756643e2f82cfc649cb839317a4db42d471ba0
ArthurG0 <agatin@umail.ucsb.edu>
2023-05-29 15:00:35 -0700

    card floats in hand

--- Assets/MyCard.cs
+++ Assets/MyCard.cs
@@ -14,11 +14,33 @@ public class MyCard : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Debug.Log("Lol Update!");
         
     }
 
-    void GrabbedCardDeck() {
-        Debug.Log("Lol I selected the card deck!");
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("card is Colliding!");
+        float offsetHeight = 0.2f;
+        // Check if the collision is with the DropOffZone
+        if (collision.gameObject.name == "DropOffZone")
+        {
+            // Disable gravity
+            Rigidbody rb = GetComponent<Rigidbody>();
+            if (rb != null)
+            {
+                rb.useGravity = false;
+                rb.velocity = new Vector3(0,0,0);
+                rb.angularVelocity = new Vector3(0,0,0);
+            }
+
+            // Adjust position above the DropOffZone
+            Vector3 newPosition = collision.transform.position + Vector3.up * offsetHeight;
+            transform.position = newPosition;
+            transform.eulerAngles = new Vector3(75,0,0);
+
+
+
+        }
     }
+
 }
